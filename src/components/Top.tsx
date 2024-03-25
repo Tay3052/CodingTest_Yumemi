6b85d76022113d4bd386c63bde65f09111bf2cf8
Takumi <fd3s.bcnr33@gmail.com>
2024-03-25 09:09:25 +0900

    Last commit

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -6,6 +6,31 @@ import { AxiosResponse } from "axios";
 
 const Top = () => {
   const [prefectures, setPrefectures] = useState<getPref[] | null>(null);
+  const [checkedPref, setCheckedPref] = useState([] as string[]);
+
+  const handleCheck = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // なぜか始めはnullになる
+    if (e.target.checked) {
+      if (checkedPref != null) {
+        setCheckedPref([...checkedPref, value]);
+
+        console.log(checkedPref, value);
+      } else {
+        setCheckedPref([value]);
+        console.log([value], "1st");
+      }
+    } else {
+      if (checkedPref) {
+        setCheckedPref(checkedPref.filter((pref) => pref !== value));
+      }
+    }
+  };
+
+  const sendProps = () => {
+    const props = checkedPref;
+    return props;
+  };
 
   const prefLists = async (): Promise<getPref[]> => {
     try {
@@ -29,14 +54,18 @@ const Top = () => {
     <>
       <div>
         <h2>都道府県一覧</h2>
-
         <table>
           <tbody>
             {prefectures &&
               prefectures.result.map((pref, index) => (
                 <tr key={pref.prefCode}>
                   <td>
-                    <input type="checkbox" name="pref" value={pref.prefCode} />
+                    <input
+                      type="checkbox"
+                      name="pref"
+                      value={pref.prefCode}
+                      onChange={handleCheck}
+                    />
                   </td>
                   <td>{pref.prefName}</td>
                 </tr>
